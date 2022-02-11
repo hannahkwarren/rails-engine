422520d5f070aaceadccd3705e591768aaf87cd9
Hannah Warren <hannahkwarren@gmail.com>
2022-02-11 11:24:13 -0500

    Oops, remove commented/unused code

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
-  has_many :invoices, through: :invoice_items, dependent: :destroy
-
-  # after_destroy :delete_invoice_too
+  has_many :invoices, through: :invoice_items
 
   include SearchModule
-
-  # private
-  # def delete_invoice_too 
-  #   self.invoices.each do |inv|
-  #     if inv.invoice_items.count == 1
-  #       inv.destroy
-  #     else
-  #       return "No invoices to delete."
-  #     end
-  #   end
-  # end
 
 end
